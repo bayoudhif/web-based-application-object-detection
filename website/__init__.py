d99a4c6c5af1bd4cf9746268e26ec242f50e1e11
Firas Bayoudhi <83510162+bayoudhif@users.noreply.github.com>
2023-02-27 21:07:32 +0000

    update

--- website/__init__.py
+++ website/__init__.py
@@ -1,40 +1,41 @@
-
-from flask import Flask, redirect,request,url_for,flash
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
-from flask_login import LoginManager,current_user
+from flask_login import LoginManager, current_user
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    db.init_app(app)
+  app = Flask(__name__)
+  app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+  app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+  db.init_app(app)
 
-    
-    from .views import views
-    from .auth import auth
+  from .views import views
+  from .auth import auth
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+  app.register_blueprint(views, url_prefix='/')
+  app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User
+  from .models import User
 
-    create_database(app)
+  with app.app_context():
+    create_database()
 
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
+  login_manager = LoginManager()
+  login_manager.login_view = 'auth.login'
+  login_manager.init_app(app)
 
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
-    return app
+  @login_manager.user_loader
+  def load_user(id):
+    return User.query.get(int(id))
+
+  return app
 
 
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
+def create_database():
+  if not path.exists('website/' + DB_NAME):
+    db.create_all()
+    print('Created Database!')
